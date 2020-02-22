8d1149a121951ecd577d17090f5e8e0b5128d26d
songtiff <ntruong30@horizon.csueastbay.edu>
2020-02-21 20:59:34 -0800

    first flashcard

---
+++ FlashcardsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashcardsApp
+//
+//  Created by Tiffany  on 2/15/20.
+//  Copyright © 2020 Tiffany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
