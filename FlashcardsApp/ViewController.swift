809a74841afccffe1f8ed70c728535119ad80a33
songtiff <ntruong30@horizon.csueastbay.edu>
2020-02-21 21:08:42 -0800

    trying to set code for first set of flashcards

--- FlashcardsApp/ViewController.swift
+++ FlashcardsApp/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerPie: UILabel!
+    @IBOutlet weak var questionPie: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        isHidden = true
+    }
+    
 }
 
