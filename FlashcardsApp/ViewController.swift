5d2dc8d22c4428d7367ad5e6cb82719138614142
songtiff <ntruong30@horizon.csueastbay.edu>
2020-03-07 11:25:09 -0800

    multiple options

--- FlashcardsApp/ViewController.swift
+++ FlashcardsApp/ViewController.swift
@@ -13,14 +13,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerPie: UILabel!
     @IBOutlet weak var questionPie: UILabel!
     @IBOutlet weak var card: UIView!
+    @IBOutlet weak var firstOptionCard: UIButton!
+    @IBOutlet weak var secondOptionCard: UIButton!
+    @IBOutlet weak var thirdOptionCard: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         card.layer.cornerRadius = 20.0
-        card.clipsToBounds = true
         card.layer.shadowRadius = 15.0
-        card.layer.shadowOpacity = 0.2
+        card.layer.shadowOpacity = 4.0
+        card.clipsToBounds = true
+        
+        firstOptionCard.layer.cornerRadius = 20.0
+        firstOptionCard.layer.borderWidth = 3.0
+        firstOptionCard.layer.borderColor = #colorLiteral(red: 1, green: 0.7436705232, blue: 0.480723083, alpha: 1)
+        firstOptionCard.layer.shadowRadius = 15.0
+        firstOptionCard.layer.shadowOpacity = 4.0
+        firstOptionCard.clipsToBounds = true
+        
+        secondOptionCard.layer.cornerRadius = 20.0
+        secondOptionCard.layer.borderWidth = 3.0
+        secondOptionCard.layer.borderColor = #colorLiteral(red: 1, green: 0.7436705232, blue: 0.480723083, alpha: 1)
+        secondOptionCard.layer.shadowRadius = 15.0
+        secondOptionCard.layer.shadowOpacity = 4.0
+        secondOptionCard.clipsToBounds = true
+        
+        thirdOptionCard.layer.cornerRadius = 20.0
+        thirdOptionCard.layer.borderWidth = 3.0
+        thirdOptionCard.layer.borderColor = #colorLiteral(red: 1, green: 0.7436705232, blue: 0.480723083, alpha: 1)
+        thirdOptionCard.layer.shadowRadius = 15.0
+        thirdOptionCard.layer.shadowOpacity = 4.0
+        thirdOptionCard.clipsToBounds = true
+
+        
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
@@ -34,6 +60,7 @@ class ViewController: UIViewController {
             answerPie.isHidden = false
         }
     }
+    
     func updateFlashcard(question: String, answer: String) {
         
     }
